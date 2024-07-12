24fd61fa5febc9022b28c9fa8e522cd467214484
yamato3069 <inouey@analix.co.jp>
2024-07-12 17:21:21 +0900

    add:いのうえ

--- src/main/java/com/example/demo/service/UserRegistrationService.java
+++ src/main/java/com/example/demo/service/UserRegistrationService.java
@@ -1,5 +1,22 @@
 package com.example.demo.service;
 
-public class UserRegistrationService {
+import java.security.SecureRandom;
 
-}
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserRegistrationService {
+		
+	    private  final SecureRandom random = new SecureRandom();
+
+	    public  String generateUserID(int length) {
+	        StringBuilder id = new StringBuilder(length);
+	        for (int i = 0; i < length; i++) {
+	            int digit = random.nextInt(10); // 0から9までのランダムな数値を生成
+	            id.append(digit);
+	        }
+	        return id.toString();
+	    }
+
+	}
+
