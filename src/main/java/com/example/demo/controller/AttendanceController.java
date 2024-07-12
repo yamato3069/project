87545e3cc94a5f452cf152efb9a16e647f980412
t-sakai <sakait@analix.co.jp>
2024-07-12 17:11:51 +0900

    add

--- src/main/java/com/example/demo/controller/AttendanceController.java
+++ src/main/java/com/example/demo/controller/AttendanceController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.Calendar;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +14,25 @@ public class AttendanceController {
     @RequestMapping("/regist")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 //        return String.format("Hello %s!", name);
+    	//Calendarクラスのオブジェクトを生成する
+        Calendar cl = Calendar.getInstance();
+        
+        System.out.println("YEAR：" + cl.get(Calendar.YEAR));
+        System.out.println("MONTH：" + cl.get(Calendar.MONTH)); 
+        System.out.println("DATE：" + cl.get(Calendar.DATE));  
     	  return "attendance/regist";
+    }
+    
+    @RequestMapping("/regist2")
+    public String hello2(@RequestParam(value = "name", defaultValue = "World") String name) {
+//        return String.format("Hello %s!", name);
+    	//Calendarクラスのオブジェクトを生成する
+        Calendar cl = Calendar.getInstance();
+        
+        System.out.println("YEAR：" + cl.get(Calendar.YEAR));
+        System.out.println("MONTH：" + cl.get(Calendar.MONTH)); 
+        System.out.println("DATE：" + cl.get(Calendar.DATE));  
+    	  return "attendance/regist2";
     }
 }
 
