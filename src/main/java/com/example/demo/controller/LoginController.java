e9d10d1285e6450b88b30e3401367ff9eedaa932
yamato3069 <inouey@analix.co.jp>
2024-07-16 12:10:29 +0900

    add

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,15 +1,42 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.entity.LoginUser;
+import com.example.demo.form.LoginForm;
+import com.example.demo.service.LoginService;
 
 @Controller
 @RequestMapping("/")
 public class LoginController {
+	@Autowired
+	private LoginService loginService;
 	
 	@RequestMapping("")
-	public String login() {
+	public String login(@ModelAttribute LoginForm loginForm) {
 		return "login/login";
+	}
+	
+	@RequestMapping("login")
+	public String loginUser(@ModelAttribute LoginForm loginForm, Model model) {
+		LoginUser user = loginService.getLoginUsers(loginForm.getId());
+		
+		System.out.print(loginService.getLoginUsers(loginForm.getId()));
+		System.out.print(loginForm);
+		
+		if( user != null && user.getPassword().equals(loginForm.getPassword())) {
+			model.addAttribute("user", user);
+			return "attendance/regist2";
+			
+		}else {
+			
+			model.addAttribute("error", "ユーザーIDまたはパスワードが正しくありません");
+		return "login/login";
+		}
 	}
 
 }
